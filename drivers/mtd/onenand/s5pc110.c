734745e843937bd4e8ed9c5973b5da072e7e4e3d
uplusplus <yjcpui@gmail.com>
2014-01-16 09:09:11 +0800

    [M] 修正sdhci一个bug.  修改系统分区表。

--- drivers/mtd/onenand/s5pc110.c
+++ drivers/mtd/onenand/s5pc110.c
@@ -89,6 +89,46 @@ struct onenand_info {
 	struct onenand_chip		onenand;
 };
 
+// struct mtd_partition s3c_partition_info[] = {
+// 	{
+// 		.name		= "misc",
+// 		.offset		= (768*SZ_1K),          /* for bootloader */
+// 		.size		= (256*SZ_1K),
+// 		.mask_flags	= MTD_CAP_NANDFLASH,
+// 	},
+// 	{
+// 		.name		= "recovery",
+// 		.offset		= MTDPART_OFS_APPEND,
+// 		.size		= (5*SZ_1M),
+// 	},
+// 	{
+// 		.name		= "kernel",
+// 		.offset		= MTDPART_OFS_APPEND,
+// 		.size		= (5*SZ_1M),
+// 	},
+// 	{
+// 		.name		= "ramdisk",
+// 		.offset		= MTDPART_OFS_APPEND,
+// 		.size		= (3*SZ_1M),
+// 	},
+// 	{
+// 		.name		= "system",
+// 		.offset		= MTDPART_OFS_APPEND,
+// 		.size		= (90*SZ_1M),
+// 	},
+// 	{
+// 		.name		= "cache",
+// 		.offset		= MTDPART_OFS_APPEND,
+// 		.size		= (80*SZ_1M),
+// 	},
+// 	{
+// 		.name		= "userdata",
+// 		.offset		= MTDPART_OFS_APPEND,
+// 		.size		= MTDPART_SIZ_FULL,
+// 	}
+// };
+
+
 struct mtd_partition s3c_partition_info[] = {
 	{
 		.name		= "misc",
@@ -99,7 +139,7 @@ struct mtd_partition s3c_partition_info[] = {
 	{
 		.name		= "recovery",
 		.offset		= MTDPART_OFS_APPEND,
-		.size		= (5*SZ_1M),
+		.size		= (1*SZ_1M),
 	},
 	{
 		.name		= "kernel",
@@ -114,12 +154,12 @@ struct mtd_partition s3c_partition_info[] = {
 	{
 		.name		= "system",
 		.offset		= MTDPART_OFS_APPEND,
-		.size		= (90*SZ_1M),
+		.size		= (150*SZ_1M),
 	},
 	{
 		.name		= "cache",
 		.offset		= MTDPART_OFS_APPEND,
-		.size		= (80*SZ_1M),
+		.size		= (20*SZ_1M),
 	},
 	{
 		.name		= "userdata",
